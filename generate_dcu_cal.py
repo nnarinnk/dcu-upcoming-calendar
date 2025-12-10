d1f06116242001b582d6f07ecc226792cd0dea5d
nnarinnk <narinkhamkaew@gmail.com>
2025-12-10 10:00:21 +0700

    Update generate_dcu_cal.py

--- generate_dcu_cal.py
+++ generate_dcu_cal.py
@@ -3,20 +3,30 @@ import os
 import requests
 from icalendar import Calendar, Event
 import datetime
+from datetime import date
 
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
 
 if not TMDB_API_KEY:
     raise ValueError("TMDB_API_KEY environment variable not set. Please set it in GitHub Secrets.")
 
-DC_STUDIOS_ID = 128064 # Company ID ของ DC Studios (ใช้สำหรับกรองข้อมูล)
+DC_COMPANY_IDS = "128064|9993"
 
 # 1. ฟังก์ชันดึงข้อมูลจาก TMDB (ตัวอย่าง: Upcoming Movies)
 def fetch_dcu_movies():
-    url = f"https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&with_companies={DC_STUDIOS_ID}&sort_by=primary_release_date.asc"
+    # หาวันที่ปัจจุบัน รูปแบบ YYYY-MM-DD
+    today = date.today().strftime("%Y-%m-%d")
+    
+    # เพิ่ม primary_release_date.gte เพื่อเอาเฉพาะหนังในอนาคต
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&with_companies={DC_COMPANY_IDS}&sort_by=primary_release_date.asc&primary_release_date.gte={today}"
+    
     response = requests.get(url)
-    data = response.json()
-    return data.get('results', [])
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('results', [])
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return []
 
 # 2. ฟังก์ชันสร้างไฟล์ iCal
 def create_ical(movies):
